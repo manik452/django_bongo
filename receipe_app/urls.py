b306756ce4cfd12ca7f1e1ef76370ac6e8f9ab2d
manikdcastalia <manik@dcastalia.com.bd>
2024-12-03 22:56:14 +0600

    Code Formatting

--- receipe_app/urls.py
+++ receipe_app/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    path('', views.receipe_home),
-    path('login', views.login_page),
-    path('logout', views.logout_page),
-    path('register', views.register),
-    path('delete/<id>', views.delete_receipe),
-
+    path("", views.receipe_home),
+    path("login", views.login_page),
+    path("logout", views.logout_page),
+    path("register", views.register),
+    path("delete/<id>", views.delete_receipe),
 ]
